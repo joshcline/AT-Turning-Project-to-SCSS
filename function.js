e3298c462c85b71b7b97f2cb8b8b0227c7acd159
Josh Cline <joshccline@outlook.com>
2023-04-04 21:24:48 -0400

    Added another functon for the backend to give the pieces a class of king, and remove pieces in the front-end to make the pieces disappear after jumping them.

--- function.js
+++ function.js
@@ -257,4 +257,29 @@ function makeMove(number) {
     }
 }
 
+function changeData(indexOfBoardPiece, modifiedIndex, removePiece) {
+    board[indexOfBoardPiece] = null;
+    board[modifiedIndex] = parseInt(selectedPiece.pieceId)
+    if (turn && selectedPiece.pieceId < 12 && modifiedIndex >= 57) {
+        document.getElementById(selectedPiece.pieceId).classList.add("king");
+    }
+    if (turn === false && selectedPiece.pieceId >= 12 && modifiedIndex <= 7) {
+        document.getElementById(selectedPiece.pieceId).classList.add("king");
+    }
+    if (removePiece) {
+        board[removePiece] = null;
+        if (turn && selectedPiece.pieceId < 12) {
+            cells[removePiece].innerHTML = "";
+            whiteScore--
+        }
+        if (turn === false && selectedPiece.pieceId >= 12) {
+            cells[removePiece].innerHTML = "";
+            blackScore--
+        }
+    }
+    resetSelectedPieceProperties();
+    removeCellonClick();
+    removeEventListener();
+}
+
 givePiecesEventListeners()
